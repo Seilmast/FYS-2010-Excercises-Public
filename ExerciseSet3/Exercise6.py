06b90298b081432014f27b49ef656b9538711081
theodorross <theodor.a.ross@uit.no>
2023-02-10 11:30:32 +0100

    updating solution to task 6

--- ExerciseSet3/Exercise6.py
+++ ExerciseSet3/Exercise6.py
@@ -1,28 +1,43 @@
 import numpy as np 
-from scipy.signal import convolve2d
+# from scipy.signal import convolve2d
 import matplotlib.pyplot as plt 
 from PIL import Image
 
-img = np.asarray(Image.open('Data/moon.tif'))
-print(img.shape)
+## Load the images
+x1 = Image.open("Data/Fig0424(a)(rectangle).tif")
+x2 = Image.open("Data/Fig0425(a)(translated_rectangle).tif")
+x3 = x1.rotate(45)
+x4 = x2.rotate(45)
 
+## Take the fft of the images
+f1 = np.fft.fftshift( np.fft.fft2(x1) )
+f2 = np.fft.fftshift( np.fft.fft2(x2) )
+f3 = np.fft.fftshift( np.fft.fft2(x3) )
+f4 = np.fft.fftshift( np.fft.fft2(x4) )
 
-laplacian_kernel_4 = np.array([[0, 1, 0],
-                             [1, -4, 1],
-                             [0, 1, 0]])
+## Plot the log-intensities of the phase and magnitude
+fig,ax = plt.subplots(4,3, figsize=(5,7))
+ax[0,0].imshow( x1, cmap="gray" )
+ax[0,1].imshow( np.log(np.absolute(f1)), cmap="gray" )
+ax[0,2].imshow( np.angle(f1), cmap="gray" )
 
-laplacian_kernel_8 = np.array([[1, 1, 1],
-                             [1, -8, 1],
-                             [1, 1, 1]])
+ax[1,0].imshow( x2, cmap="gray" )
+ax[1,1].imshow( np.log(np.absolute(f2)), cmap="gray" )
+ax[1,2].imshow( np.angle(f2), cmap="gray" )
 
-img_4 = convolve2d(img, laplacian_kernel_4, mode='same')
-img_8 = convolve2d(img, laplacian_kernel_8, mode='same')
+ax[2,0].imshow( x3, cmap="gray" )
+ax[2,1].imshow( np.log(np.absolute(f3)), cmap="gray" )
+ax[2,2].imshow( np.angle(f3), cmap="gray" )
 
+ax[3,0].imshow( x4, cmap="gray" )
+ax[3,1].imshow( np.log(np.absolute(f4)), cmap="gray" )
+ax[3,2].imshow( np.angle(f4), cmap="gray" )
 
-fig, ax = plt.subplots(2,2,figsize=(20,10))
-ax[0,0].imshow(img, cmap='gray')
-ax[0,1].imshow(img_8, cmap='gray')
-ax[1,0].imshow(img - img_4, cmap='gray', vmin=0, vmax=255)
-ax[1,1].imshow(img - img_8, cmap='gray', vmin=0, vmax=255)
+ax[0,0].set_title("Original Image")
+ax[0,1].set_title("Magnitude of FFT")
+ax[0,2].set_title("Phase of FFT")
+for a in ax.ravel(): a.set_axis_off()
+plt.tight_layout()
 
 plt.show()
+
